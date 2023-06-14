839e856fae7010efa17d50511e5d53faf61013e7
Rute Santos <104737871+rutesantos4@users.noreply.github.com>
2023-06-14 22:35:57 +0100

    fix: comment on url
    Co-authored-by: João Freitas <joao.mag.freitas@gmail.com>

--- src/url.ts
+++ src/url.ts
@@ -3,7 +3,7 @@ const telegramApiUrl = 'https://api.telegram.org';
 /**
  * Exposes the URL endpoint for sending a message to a Telegram chat using Telegram API.
  *
- * @param token - Bot Token that will send the message.
+ * @param token - API Token of the bot that will send the message.
  * @returns An instance of the {@link URL} class that represents the endpoint for HTTP clients to consume.
  */
 export default function (token: string): URL {
